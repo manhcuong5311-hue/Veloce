41ceac7343a890633cdfe3c05aa2f6715c34add0
SamCorp <Manhcuong5311@gmail.com>
2026-04-09 10:09:01 +0700

    Initial Commit

---
+++ Veloce/VeloceApp.swift
@@ -0,0 +1,17 @@
+//
+//  VeloceApp.swift
+//  Veloce
+//
+//  Created by Sam Manh Cuong on 9/4/26.
+//
+
+import SwiftUI
+
+@main
+struct VeloceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
